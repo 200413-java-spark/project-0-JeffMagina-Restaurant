32c2424e9660f2a936bce2e981a1a5ec980080d8
JeffreyMagina <maginajeffrey@gmail.com>
2020-04-22 19:10:13 -0400

    Test case for reading from AWS server in main

--- src/main/java/com/github/jeffmagina/restaurant/main/Main.java
+++ src/main/java/com/github/jeffmagina/restaurant/main/Main.java
@@ -13,33 +13,35 @@ import com.github.jeffmagina.restaurant.customerorder.CustomerOrder;
 
 public class Main {
 	public static void main(String[] args) {
-		//Cashier cashier = new Cashier();
-		//File orderForm = new File("OrderForm.txt");
-		//IO parser = new IO();
-		
-		//Parse order
-		//CustomerOrder custOrder = new CustomerOrder();
-		//custOrder = parser.ParseCustOrderForm(orderForm);
+		Cashier cashier = new Cashier();
+		File orderForm = new File("OrderForm.txt");
+		IO parser = new IO();
 
-		// Cashier interactions with customer
-		//cashier.interaction(custOrder);
-		String url = "jdbc:postgresql://localhost:5432/jeffMagina";
+		// Parse order
+		CustomerOrder custOrder = new CustomerOrder();
+		custOrder = parser.ParseCustOrderForm(orderForm);
+
+		//Cashier interactions with customer
+		cashier.interaction(custOrder);
+		
+		//Testing reading from AWS database
+		String url = "jdbc:postgresql://3.134.83.141:5432/jeffMagina";
 		String user = "jeffMagina";
 		String password = "jeffMagina";
-		
-		try(Connection conn = DriverManager.getConnection(url,user,password);) {
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);) {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from orderhistory");
-while(rs.next()){
-			System.out.println(rs.getString("order_id"));
-			System.out.println(rs.getString("name"));
-			System.out.println(rs.getString("ordercost"));
-			System.out.println(rs.getString("paymentamount"));
-			System.out.println(rs.getString("changegiven"));
-}	
-	} catch (SQLException e) {
+			while (rs.next()) {
+				System.out.println(rs.getString("order_id"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("ordercost"));
+				System.out.println(rs.getString("paymentamount"));
+				System.out.println(rs.getString("changegiven"));
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
